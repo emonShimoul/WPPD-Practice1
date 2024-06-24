34843ccacaf031a87f51879dbcb0c016366b8b5e
emonShimoul <emonshimoul@gmail.com>
2024-06-24 15:02:26 +0600

    OOP standard coding

--- our-first-plugin.php
+++ our-first-plugin.php
@@ -8,17 +8,43 @@
  * Author URI:        https://author.example.com/
  */
 
- add_filter("the_title","wedevs_ofp_change_title");
- function wedevs_ofp_change_title($post_title) {
-    return strtoupper($post_title);
+ class Our_First_Plugin{
+    public function __construct(){
+        add_action('init', array($this,'initialize'));
+    }
+
+    function initialize(){
+        add_filter('the_title', array($this,'wedevs_ofp_change_title'));
+        add_filter('the_content', array($this,'wedevs_ofp_change_content'));
+    }
+
+    function wedevs_ofp_change_title($post_title){
+        return strtoupper($post_title);
+    }
+
+    function wedevs_ofp_change_content( $post_content ) {
+        // find word count
+        $content = strip_tags($post_content);
+        $word_count = str_word_count($content);
+    
+        $reading_time = ceil($word_count / 200);
+        return $post_content . "<p>{$word_count} words, approximate reading time = {$reading_time} minutes</p>";
+     }
  }
 
- add_filter('the_content','wedevs_ofp_change_content');
- function wedevs_ofp_change_content( $post_content ) {
-    // find word count
-    $content = strip_tags($post_content);
-    $word_count = str_word_count($content);
+ new Our_First_Plugin();
 
-    $reading_time = ceil($word_count / 200);
-    return $post_content . "<p>{$word_count} words, approximate reading time = {$reading_time} minutes</p>";
- }
+//  add_filter("the_title","wedevs_ofp_change_title");
+//  function wedevs_ofp_change_title($post_title) {
+//     return strtoupper($post_title);
+//  }
+
+//  add_filter('the_content','wedevs_ofp_change_content');
+//  function wedevs_ofp_change_content( $post_content ) {
+//     // find word count
+//     $content = strip_tags($post_content);
+//     $word_count = str_word_count($content);
+
+//     $reading_time = ceil($word_count / 200);
+//     return $post_content . "<p>{$word_count} words, approximate reading time = {$reading_time} minutes</p>";
+//  }
