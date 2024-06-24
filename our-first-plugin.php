d7c57bf91c107bbaeae3fd0de1d3988c7200ddc3
emonShimoul <emonshimoul@gmail.com>
2024-06-24 13:39:23 +0600

    initial commit

---
+++ our-first-plugin.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Plugin Name:       Our First Plugin
+ * Plugin URI:        https://example.com/plugins/our-first-plugin/
+ * Description:       This is our first plugin.
+ * Version:           1.0.0
+ * Author:            Emon Shimoul
+ * Author URI:        https://author.example.com/
+ */
+
+ add_filter("the_title","change_title");
+ function change_title($post_title) {
+    return strtoupper($post_title);
+ }
+
+ add_filter('the_content','change_content');
+ function change_content( $post_content ) {
+    // find word count
+    $content = strip_tags($post_content);
+    $word_count = str_word_count($content);
+
+    $reading_time = ceil($word_count / 200);
+    return $post_content . "<p>{$word_count} words, approximate reading time = {$reading_time} minutes</p>";
+ }
